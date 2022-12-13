0999f8bf8ddfaa0d8fd4d17c7c5985d5ebc4c369
king <king@qq.com>
2022-12-13 17:00:37 +0800

    大改！

--- xghblog-cloud-parent/xghblog-cloud-services/xghblog-cloud-testdemo/src/main/java/com/xghblog/services/demoSpring/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xghblog.services.demoSpring.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xghblog.services.demoSpring.pojo.Users;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserMapper extends BaseMapper<Users> {
-}
